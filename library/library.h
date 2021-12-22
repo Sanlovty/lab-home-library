61832fb6da51ffb6c5654c9177d7307613256bbc
Gleb <perveev@surfstudio.ru>
2021-12-22 18:26:53 +0300

    feat: Добавлены объявления основных методов класса Library.

--- library/library.h
+++ library/library.h
@@ -5,6 +5,29 @@
 class Library
 {
 protected:
+	vector<Book> books_;
 public:
+	Library(const vector<Book>& books);
+	Library();
+
+	Book at(const size_t& index) const;
+	Book front() const;
+	Book back() const;
+
+	Book insert(const size_t& index, const Book& book);
+	void erase(const size_t& index);
+
+	void pushBack(const Book& book);
+	void pushFront(const Book& book);
+
+	void popBack() const;
+	void popFront() const;
+
+	void sortByName();
+	void sortByPublishingHouse();
+	void sortByIsbn();
+	void sortByDate();
+	void sortByAuthorName();
+	void sortByAuthorDate();
 };
 #endif
