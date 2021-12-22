098ae7d88ee070d2a6c5b3bf4cc83708ce4b5f87
Gleb <perveev@surfstudio.ru>
2021-12-22 20:11:21 +0300

    docs,feat: Добавлены доки и два новых конструктора

--- library/author.h
+++ library/author.h
@@ -3,13 +3,15 @@
 
 #include "header.h"
 
-// TODO: docs
 class Author
 {
 protected:
+	// Дата рождения автора
 	date_t birthday_;
+	// ФИО автора
 	fullname_t fullName_;
 public:
+	// Конструкторы с различными параметрами:
 	Author(const date_t& birthday,
 	       const fullname_t& fullName);
 	Author(const size_t& day,
@@ -18,14 +20,27 @@ public:
 	       const string& name,
 	       const string& surname,
 	       const string& secondName);
+	Author(const date_t& birthday,
+	       const string& name,
+	       const string& surname,
+	       const string& secondName);
+	Author(const size_t& day,
+	       const size_t& month,
+	       const size_t& year,
+	       const fullname_t& fullName);
 
+	// Метод получения дня рождения
 	date_t getBirthday() const;
+	// Метод получения ФИО
 	fullname_t getFullName() const;
 
+	// Метод установки дня рождения с разными параметрами
 	void setBirthday(const size_t& day,
 	                 const size_t& month,
 	                 const size_t& year);
 	void setBirthday(const date_t& birthday);
+
+	// Метод установки ФИО с разными параметрами
 	void setFullName(const string& name,
 	                 const string& surname,
 	                 const string& secondName);
