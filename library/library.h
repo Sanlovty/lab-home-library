41cf69b20ac05eeaefc2fcac8e941286cebdd6f7
Gleb <perveev@surfstudio.ru>
2021-12-22 20:22:08 +0300

    docs: Добавлены доки для Library

--- library/library.h
+++ library/library.h
@@ -6,9 +6,15 @@
 class Library
 {
 protected:
+	// ¬ектор (массив) книг
 	vector<Book> books_;
 
+	// ћетод, провер€ющий выходит ли индекс за пределы размеров books_. ≈сли выходит,
+	// то кидаетс€ ошибка
 	void checkIndex_(const size_t& index) const;
+	// ћетоды сравнени€ двух книг (по разным параметрам), которые нужны дл€
+	// использовани€ метода sort(books.begin(), books.end()), который сам будет сортировать
+	// массивы, засовыва€ в методы по два элемента (с помощью этих методов он и будет их сравнивать)
 	static bool compareBooksName_(const Book& first, const Book& second);
 	static bool compareBooksPublishingHouse_(const Book& first, const Book& second);
 	static bool compareBooksIsbn_(const Book& first, const Book& second);
@@ -17,25 +23,38 @@ protected:
 	static bool compareBooksAuthorBirthday_(const Book& first, const Book& second);
 	static bool compareBooksAuthorFullName_(const Book& first, const Book& second);
 public:
+	//  онструкторы с параметрами / без
 	Library(const vector<Book>& books);
 	Library();
 
+	// ћетод получени€ количества книг в библиотеки
 	size_t size() const;
+	// ћетод провер€ющий, есть ли в библиотеке хот€ бы одна книга
 	bool isEmpty() const;
 
+	// ћетод возвращающий книгу по индексу
 	Book at(const size_t& index) const;
+	// ћетод возвращающий книгу в самом начале
 	Book front() const;
+	// ћетод возвращающий книгу в самом конце
 	Book back() const;
 
+	// ћетод добавл€ющий книгу (book) по индексу(index) в библиотеку 
 	void insert(const size_t& index, const Book& book);
+	// ћетод удал€ющий книгу по индексу(index) из библиотеки 
 	void erase(const size_t& index);
 
+	// ћетод добавл€ющий книгу (book) в конец библиотеки 
 	void pushBack(const Book& book);
+	// ћетод добавл€ющий книгу (book) в начало библиотеки 
 	void pushFront(const Book& book);
 
+	// ћетод удал€ющий книгу из начала библиотеки 
 	void popBack();
+	// ћетод удал€ющий книгу из конца библиотеки 
 	void popFront();
 
+	// ћетоды сортировки библиотеки книг по разным параметрам
 	void sortByName();
 	void sortByPublishingHouse();
 	void sortByIsbn();
