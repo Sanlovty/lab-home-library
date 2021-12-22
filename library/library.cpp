f20843dc12fd2a9d75e762b0b0d3611b6f651309
Gleb <perveev@surfstudio.ru>
2021-12-22 19:34:00 +0300

    feat: Реализованы абсолютно все методы класса Library

--- library/library.cpp
+++ library/library.cpp
@@ -8,6 +8,98 @@ void Library::checkIndex_(const size_t& index) const
 	}
 }
 
+bool Library::compareBooksName_(const Book& first, const Book& second)
+{
+	return first.getName().compare(second.getName()) < 0;
+}
+
+bool Library::compareBooksPublishingHouse_(const Book& first, const Book& second)
+{
+	return first.getPublishingHouse().compare(second.getPublishingHouse()) < 0;
+}
+
+bool Library::compareBooksIsbn_(const Book& first, const Book& second)
+{
+	return first.getIsbn() < second.getIsbn();
+}
+
+bool Library::compareDate_(const date_t& first, const date_t& second)
+{
+	const size_t firstDateDay = get<0>(first);
+	const size_t firstDateMonth = get<1>(first);
+	const size_t firstDateYear = get<2>(first);
+	const size_t secondDateDay = get<0>(second);
+	const size_t secondDateMonth = get<1>(second);
+	const size_t secondDateYear = get<2>(second);
+
+	if (firstDateYear < secondDateYear)
+	{
+		return true;
+	}
+	if (firstDateYear > secondDateYear)
+	{
+		return false;
+	}
+
+	if (firstDateMonth < secondDateMonth)
+	{
+		return true;
+	}
+	if (firstDateMonth > secondDateMonth)
+	{
+		return false;
+	}
+
+	if (firstDateDay < secondDateDay)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Library::compareBooksDate_(const Book& first, const Book& second)
+{
+	return compareDate_(first.getDate(), second.getDate());
+}
+
+bool Library::compareBooksAuthorBirthday_(const Book& first, const Book& second)
+{
+	return compareDate_(first.getAuthor().getBirthday(), second.getAuthor().getBirthday());
+}
+
+bool Library::compareBooksAuthorFullName_(const Book& first, const Book& second)
+{
+	const fullname_t firstDate = first.getAuthor().getFullName();
+	const fullname_t secondDate = second.getAuthor().getFullName();
+	const string firstName = get<0>(firstDate);
+	const string firstSurname = get<1>(firstDate);
+	const string firstSecondName = get<2>(firstDate);
+	const string secondName = get<0>(secondDate);
+	const string secondSurname = get<1>(secondDate);
+	const string secondSecondName = get<2>(secondDate);
+	if (firstName.compare(secondName) < 0)
+	{
+		return true;
+	}
+	if (firstName.compare(secondName) > 0)
+	{
+		return false;
+	}
+	if (firstSurname.compare(secondSurname) < 0)
+	{
+		return true;
+	}
+	if (firstSurname.compare(secondSurname) > 0)
+	{
+		return false;
+	}
+	if (firstSecondName.compare(secondSecondName) < 0)
+	{
+		return true;
+	}
+	return false;
+}
+
 Library::Library(const vector<Book>& books) :
 	books_(books)
 {
@@ -75,24 +167,30 @@ void Library::popFront()
 
 void Library::sortByName()
 {
+	sort(books_.begin(), books_.end(), compareBooksName_);
 }
 
 void Library::sortByPublishingHouse()
 {
+	sort(books_.begin(), books_.end(), compareBooksPublishingHouse_);
 }
 
 void Library::sortByIsbn()
 {
+	sort(books_.begin(), books_.end(), compareBooksIsbn_);
 }
 
 void Library::sortByDate()
 {
+	sort(books_.begin(), books_.end(), compareBooksDate_);
 }
 
-void Library::sortByAuthorName()
+void Library::sortByAuthorFullName()
 {
+	sort(books_.begin(), books_.end(), compareBooksAuthorFullName_);
 }
 
-void Library::sortByAuthorDate()
+void Library::sortByAuthorBirthday()
 {
+	sort(books_.begin(), books_.end(), compareBooksAuthorBirthday_);
 }
--- library/library.h
+++ library/library.h
@@ -2,12 +2,20 @@
 #define LIBRARY_H
 #include "book.h"
 
+// TODO : docs
 class Library
 {
 protected:
 	vector<Book> books_;
 
 	void checkIndex_(const size_t& index) const;
+	static bool compareBooksName_(const Book& first, const Book& second);
+	static bool compareBooksPublishingHouse_(const Book& first, const Book& second);
+	static bool compareBooksIsbn_(const Book& first, const Book& second);
+	static bool compareDate_(const date_t& first, const date_t& second);
+	static bool compareBooksDate_(const Book& first, const Book& second);
+	static bool compareBooksAuthorBirthday_(const Book& first, const Book& second);
+	static bool compareBooksAuthorFullName_(const Book& first, const Book& second);
 public:
 	Library(const vector<Book>& books);
 	Library();
@@ -32,7 +40,7 @@ public:
 	void sortByPublishingHouse();
 	void sortByIsbn();
 	void sortByDate();
-	void sortByAuthorName();
-	void sortByAuthorDate();
+	void sortByAuthorFullName();
+	void sortByAuthorBirthday();
 };
 #endif
