79b1a769543dd2f52a44d45187da733311bb241b
Gleb <perveev@surfstudio.ru>
2021-12-22 17:31:20 +0300

    feat: Добавлен файл зоголовк
    В нё

---
+++ library/header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <string>
+#include <tuple>
+using std::string;
+
+template <typename _Ty1, typename _Ty2, typename _Ty3>
+using triple_t = std::tuple<_Ty1, _Ty2, _Ty3>;
+
+using date_t = triple_t<size_t, size_t, size_t>;
+using fullname_t = triple_t<string, string, string>;
+#endif
